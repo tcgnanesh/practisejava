ba1fecc56b36bc56c4e0fb5c7aac98350524a252
tcgnanesh <tcgnanesh8@gmail.com>
2026-08-17 20:14:34 +0530

    day1 votepgm

---
+++ vote.java
@@ -0,0 +1,13 @@
+package practisejava.vote;
+
+public class vote {
+    public static void main(String args[]) {
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("eligible for vote");
+        } else {
+            System.out.println("not eligible for vote");
+        }
+    }
+}
