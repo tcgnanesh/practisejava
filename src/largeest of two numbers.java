21e0f82c736eb9bc2b7862e90f60c4f74e0b3a09
tcgnanesh <tcgnanesh8@gmail.com>
2026-08-19 18:52:50 +0530

    print1to10

---
+++ src/largeest of two numbers.java
@@ -0,0 +1,11 @@
+public class largestoftwonumbers {
+    public static void main(String [] args){
+        int a=50;
+        int b=60;
+        if (a > b){
+            System.out.println("a is largest");
+            } else{
+                System.out.println("b is largest number");
+            }
+        }
+}      
