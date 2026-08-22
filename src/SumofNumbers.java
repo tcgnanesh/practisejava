6ec6b0fb52231a97edc252e4a7265437fe607393
tcgnanesh <tcgnanesh8@gmail.com>
2026-08-22 18:52:09 +0530

    pattern

--- src/SumofNumbers.java
+++ src/SumofNumbers.java
@@ -1,9 +1,9 @@
 public class SumofNumbers {
     public static void main(String[] args) {
 
-        int sum = 0;
+        int sum = 5;
 
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             sum = sum + i;
         }
 
