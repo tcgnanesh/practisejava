0eac8daa46e29fddb1369b6dd981c9da76c0067f
tcgnanesh <tcgnanesh8@gmail.com>
2026-08-25 20:40:01 +0530

    biggestnumber

---
+++ src/biggestnumber.java
@@ -0,0 +1,13 @@
+public class BiggestNumber {
+    public static void main(String[] args) {
+
+        int a = 85;
+        int b = 42;
+
+        if (a > b) {
+            System.out.println("Biggest = " + a);
+        } else {
+            System.out.println("Biggest = " + b);
+        }
+    }
+}
