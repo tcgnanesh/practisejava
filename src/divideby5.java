69833d0d7cb3e9793c64aadfe285d9040a268ec3
tcgnanesh <tcgnanesh8@gmail.com>
2026-09-01 19:59:33 +0530

    divisible

---
+++ src/divideby5.java
@@ -0,0 +1,12 @@
+public class Divisible {
+    public static void main(String[] args) {
+
+        int num = 35;
+
+        if (num % 5 == 0) {
+            System.out.println("Divisible by 5");
+        } else {
+            System.out.println("Not Divisible by 5");
+        }
+    }
+}
